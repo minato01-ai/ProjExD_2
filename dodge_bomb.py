5133413f1e69d87560ed2d735c2aba61483dd8bb
minato01-ai <hom452063@gmail.com>
2026-06-16 14:28:53 +0900

    練習問題2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,16 +1,16 @@
 import os
+import random
 import sys
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
-DELTA={
-    pg.K_UP:(0,-5),
-    pg.K_DOWN:(0,+5),
-    pg.K_LEFT:(-5,0),
-    pg.K_RIGHT:(+5,0),
-       
-       }
+DELTA = {
+    pg.K_UP: (0, -5),  # 上矢印キー
+    pg.K_DOWN: (0, +5),  # 下矢印キー
+    pg.K_LEFT: (-5, 0),  # 左矢印キー
+    pg.K_RIGHT: (+5, 0),  # 右矢印キー
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -18,9 +18,20 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
+    # こうかとんの初期化
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    # 爆弾の初期化
+    bb_img = pg.Surface((20, 20))  # 爆弾用の空のSurface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 半径10の赤い円を描画
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    bb_rct.centerx = random.randint(0, WIDTH)  # 横初期座標
+    bb_rct.centery = random.randint(0, HEIGHT)  # 縦初期座標
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -31,21 +42,23 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        #if key_lst[pg.K_UP]:
-        #    sum_mv[1] -= 5
-        #if key_lst[pg.K_DOWN]:
-        #    sum_mv[1] += 5
-        #if key_lst[pg.K_LEFT]:
-        #    sum_mv[0] -= 5
-        #if key_lst[pg.K_RIGHT]:
-        #    sum_mv[0] += 5
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
         for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0]+=mv[0]
-                sum_mv[1]+=mv[1]
-
+                sum_mv[0] += mv[0]  # 横方向の移動量
+                sum_mv[1] += mv[1]  # 縦方向の移動量
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
