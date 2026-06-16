b29c1ef80b5081fe8fc2232fcc4eb24ec32ddf9b
minato01-ai <hom452063@gmail.com>
2026-06-16 14:52:29 +0900

    練習問題4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -52,6 +52,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):
+            print("ゲームオーバー")
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
