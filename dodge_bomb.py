385ed40f9e0251652c0939ee1dee094292a7dbd2
minato01-ai <hom452063@gmail.com>
2026-06-16 18:08:08 +0900

    コメント修正2回目#1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -71,7 +71,7 @@ def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
     for r in range(1, 11):
         bb_img = pg.Surface((20*r, 20*r))
         pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
-        bb_img.set_colorkey((0, 0, 0)) # 背景の黒を透過
+        bb_img.set_colorkey((0, 0, 0)) 
         bb_imgs.append(bb_img)
     return bb_imgs, bb_accs
 
@@ -79,33 +79,38 @@ def get_kk_imgs() -> dict[tuple[int, int], pg.Surface]:
     """
     移動量タプルと対応するこうかとん画像Surfaceの辞書を返す
     """
-    img0 = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9) # 左向き（元の画像）
-    img1 = pg.transform.flip(img0, True, False) # 右向き（左右反転）
+    img0 = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9) 
+    img1 = pg.transform.flip(img0, True, False) 
 
+
+    """
+    こうかとんの向きの定義
+    """
     return {
-        (0, 0): img0,  # キー押下がない場合（デフォルトの向き）
-        (+5, 0): img1, # 右
-        (+5, -5): pg.transform.rotozoom(img1, 45, 1.0),  # 右上
-        (0, -5): pg.transform.rotozoom(img1, 90, 1.0),   # 上
-        (-5, -5): pg.transform.rotozoom(img0, -45, 1.0), # 左上
-        (-5, 0): img0, # 左
-        (-5, +5): pg.transform.rotozoom(img0, 45, 1.0),  # 左下
-        (0, +5): pg.transform.rotozoom(img1, -90, 1.0),  # 下
-        (+5, +5): pg.transform.rotozoom(img1, -45, 1.0), # 右下
-    }
-
+        (0, 0): img0,
+        (+5, 0): img1, 
+        (+5, -5): pg.transform.rotozoom(img1, 45, 1.0),  
+        (0, -5): pg.transform.rotozoom(img1, 90, 1.0),   
+        (-5, -5): pg.transform.rotozoom(img0, -45, 1.0),
+        (-5, 0): img0, 
+        (-5, +5): pg.transform.rotozoom(img0, 45, 1.0),  
+        (0, +5): pg.transform.rotozoom(img1, -90, 1.0),  
+        (+5, +5): pg.transform.rotozoom(img1, -45, 1.0),
+    }    
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
-    
-    # こうかとん画像の初期化（辞書を取得）
+    """
+    こうかとん画像の初期化（辞書を取得）
+    """
     kk_imgs = get_kk_imgs()
     kk_img = kk_imgs[(0, 0)] 
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-
-    # 爆弾の初期化
+    """
+    爆弾の初期化
+    """
     bb_imgs, bb_accs = init_bb_imgs()
     bb_img = bb_imgs[0]
     bb_rct = bb_img.get_rect() 
@@ -134,22 +139,26 @@ def main():
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-            
-        # 飛ぶ方向に従ってこうかとん画像を切り替える
+        """
+        飛ぶ方向に従ってこうかとん画像を切り替える
+        """
         kk_img = kk_imgs[tuple(sum_mv)]
         screen.blit(kk_img, kk_rct)
-
-        # 段階（0〜9）の計算
+        """
+        段階（0〜9）の計算
+        """
         step = min(tmr // 500, 9)
-        
-        # 爆弾の画像、Rectサイズ、加速度を更新
+        """
+        爆弾の画像、Rectサイズ、加速度を更新
+        """
         bb_img = bb_imgs[step]
         bb_rct.width = bb_img.get_rect().width
         bb_rct.height = bb_img.get_rect().height
         avx = vx * bb_accs[step]
         avy = vy * bb_accs[step]
-
-        # 計算した加速度(avx, avy)で移動
+        """
+        計算した加速度(avx, avy)で移動
+        """
         bb_rct.move_ip(avx, avy)
         
         yoko, tate = check_bound(bb_rct)
