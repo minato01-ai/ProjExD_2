f61c2afdcedc79248048549dedeeb31500df9cf0
minato01-ai <hom452063@gmail.com>
2026-06-16 16:25:15 +0900

    追加機能１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,50 +2,87 @@ import os
 import random
 import sys
 import pygame as pg
-
+import time
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
-    pg.K_UP: (0, -5),  # 上矢印キー
-    pg.K_DOWN: (0, +5),  # 下矢印キー
-    pg.K_LEFT: (-5, 0),  # 左矢印キー
-    pg.K_RIGHT: (+5, 0),  # 右矢印キー
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """
-    引数：こうかとんRect or 爆弾Rect
-    戻り値：判定結果タプル（横方向判定結果，縦方向判定結果）
-    True：画面内／False：画面外
+    引数：効果トンRect or 爆弾Rect
+    戻り値：判定結果タプル（後方向判定結果、 縦方向判定結果）
+    True：画面内/False：画面外
     """
-    yoko, tate = True, True
-    if rct.left < 0 or WIDTH < rct.right:  # 横方向判定
-        yoko = False
-    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向判定
-        tate = False
-    return yoko, tate
 
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate        
+
+def gameover(screen: pg.Surface) -> None:
+    """
+    ゲームオーバー画面を5秒間表示する
+    """
+    # 黒い半透明の画面
+    black = pg.Surface((WIDTH, HEIGHT))
+    black.fill((0, 0, 0))
+    black.set_alpha(200)
+
+    # Game Over の文字
+    font = pg.font.Font(None, 80)
+    txt = font.render("Game Over", True, (255, 255, 255))
+    txt_rct = txt.get_rect(center=(WIDTH//2, HEIGHT//2 + 10))
+
+    # 泣いているこうかとん
+    cry_img = pg.transform.rotozoom(
+        pg.image.load("fig/8.png"), 0, 1.0
+    )
+    
+    left_rct = cry_img.get_rect(
+        center = (WIDTH//2 - 200, HEIGHT//2)
+    )
+
+    right_rct = cry_img.get_rect(
+        center = (WIDTH//2 + 200, HEIGHT//2)
+    )
+
+    # Surfaceへ描画
+    black.blit(txt, txt_rct)
+    black.blit(cry_img, left_rct)
+    black.blit(cry_img, right_rct)
+
+    # 画面へ表示
+    screen.blit(black, (0, 0))
+    pg.display.update()
+
+    # 5秒停止
+    time.sleep(5)
+    
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
-    # こうかとんの初期化
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
 
-    # 爆弾の初期化
-    bb_img = pg.Surface((20, 20))  # 爆弾用の空のSurface
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 半径10の赤い円を描画
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_img.set_colorkey((0, 0, 0))
-    bb_rct = bb_img.get_rect()  # 爆弾Rect
-    bb_rct.centerx = random.randint(0, WIDTH)  # 横初期座標
-    bb_rct.centery = random.randint(0, HEIGHT)  # 縦初期座標
+    bb_rct = bb_img.get_rect() 
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
     vx, vy = +5, +5
-
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -53,7 +90,8 @@ def main():
             if event.type == pg.QUIT: 
                 return
         if kk_rct.colliderect(bb_rct):
-            print("ゲームオーバー")
+            gameover(screen)
+            return
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -68,18 +106,19 @@ def main():
         #     sum_mv[0] += 5
         for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += mv[0]  # 横方向の移動量
-                sum_mv[1] += mv[1]  # 縦方向の移動量
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  # 動きをなかったことにする
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
 
         bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(bb_rct)
-        if not yoko:  # 横方向にはみ出ていたら
+        if not yoko:
             vx *= -1
-        if not tate:  # 縦方向にはみ出ていたら
+        if not tate:
             vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
